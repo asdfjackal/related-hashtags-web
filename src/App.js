c7857c440b6a149804965da22b00180c5560ba3e
Aleksandr <aleksandr@asdfjackal.com>
2017-09-04 17:46:50 -0400

    Added initial UI frame

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './normalize.css';
+import './skeleton.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <br />
+        <div className="row">
+          <h2>Related Hashtag Search</h2>
+          <p>Enter a hashtag and this site will find other hashtags used with the tag your are searching for.</p>
+          <p>Search is done using what twitter has determined to be the 100 most relevant tweets associated with the given hashtag from the past 7 days.</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="row">
+          <div className="ten columns">
+            <input className="u-max-full-width u-full-width" type="text" placeholder="Enter hashtag here (including the # symbol)"/>
+          </div>
+          <div className="two columns">
+            <button className="button-primary">Run Search</button>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <table className="u-full-width">
+            <thead>
+              <tr>
+                <th>Hashtag</th>
+                <th>Instances</th>
+                <th>Search</th>
+                <th>Combined Search</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>#netneutrality</td>
+                <td>99</td>
+                <td><a href="/">Link</a></td>
+                <td><a href="/">Link</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
